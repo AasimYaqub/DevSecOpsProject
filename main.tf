5f22f7e777d4b845b63afc52643ac16cfa4c09e2
Aasim Yaqub <aasimyaqub@gmail.com>
2025-01-11 11:37:17 +0000

    update

--- main.tf
+++ main.tf
@@ -1,6 +1,15 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
+
 resource "aws_s3_bucket" "terraform-nov-project23" {
   bucket = "terraform-nov-project23"
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "terraform-nov-project23"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
   }
+}
